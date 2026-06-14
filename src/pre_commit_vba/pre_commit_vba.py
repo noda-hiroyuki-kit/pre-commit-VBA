8fe0bd53d1a6b19b2d71ed89427dadeb36ddf141
NODA HIROYUKI <noda.hiroyuki@yk.commufa.jp>
2026-06-14 18:51:16 +0900

    chore: release v0.3.5

--- src/pre_commit_vba/pre_commit_vba.py
+++ src/pre_commit_vba/pre_commit_vba.py
@@ -24,7 +24,7 @@ from zipfile import BadZipFile, ZipFile
 import typer
 from win32com.client import DispatchEx
 
-__version__ = "0.3.4"
+__version__ = "0.3.5"
 
 
 class UndefineTypeError(Exception):
